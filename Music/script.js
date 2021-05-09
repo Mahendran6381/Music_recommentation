43b529a58f8ea89ae19b6ee029e69146ef91fcfd
Mahi <mahendran638161@gmail.com>
2021-05-09 08:05:47 +0530

    file added

---
+++ Music/script.js
@@ -0,0 +1,41 @@
+var ID;
+
+$("#happy").click(function(e){
+      ID = 'happy songs'
+  });
+$("#love").click(function(){
+    ID = 'Lovesongs hollywood'
+  });
+$("#angry").click(function(){
+    ID ='Angry management'
+  });
+$("#nothing").click(function(){
+   ID ='Motivated songs'
+  });
+  var car;
+
+  setTimeout(function(){
+      var API_KEY = "AIzaSyCnvQYBiAZW-CFlHdpB2o9eBkeisPlMl2M"
+      var search  = ID
+      var MAX_RESULT = 10
+      var video =''
+      
+      
+      videoSearch(API_KEY,search,MAX_RESULT,video)
+            
+  },3000)
+
+  function videoSearch(key,search,max,video){
+       $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key="+key+"&maxResults"+max+"&type=video&q="+search,
+       function(data){
+           console.log(data)
+           data.items.forEach(item => {
+               video =`
+               <iframe width="420" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+               
+               `
+               $('#videos').append(video)
+               
+           });
+       });
+    }
